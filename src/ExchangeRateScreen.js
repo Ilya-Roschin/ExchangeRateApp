6f8f2f5f56410a817e88d2f7d9baa6c64350fd5b
ILYA <roshhin-78@mail.ru>
2023-03-12 17:28:23 +0300

    refactor app

--- src/ExchangeRateScreen.js
+++ src/ExchangeRateScreen.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
 import axios from 'axios';
 import { SvgUri } from 'react-native-svg';
+import { Picker } from '@react-native-picker/picker';
 
 const ExchangeRateScreen = () => {
   const [exchangeRate, setExchangeRate] = useState(null);
   const [amount, setAmount] = useState('');
+  const [selectedCurrency, setSelectedCurrency] = useState('USD');
   const [convertedAmount, setConvertedAmount] = useState('');
 
   useEffect(() => {
@@ -17,8 +19,16 @@ const ExchangeRateScreen = () => {
 
   const handleAmountChange = (value) => {
     setAmount(value);
-    const convertedValue = parseFloat(value) * exchangeRate.USD_out;
+    rate = exchangeRate[selectedCurrency + '_out'];
+    if (selectedCurrency == 'RUB') {
+      rate = rate / 100;
+    }
+    const convertedValue = parseFloat(value) * rate;
     setConvertedAmount(convertedValue.toFixed(2));
+  }
+
+  const handleCurrencyChange = (value) => {
+    setSelectedCurrency(value);
   }
 
   return (
@@ -26,18 +36,27 @@ const ExchangeRateScreen = () => {
       {exchangeRate ? (
         <>
           <Text style={styles.text}>
+            Exchange rates for {exchangeRate.date}:
+          </Text>
+          <Text style={styles.text}>
             USD/BYN: {exchangeRate.USD_in} / {exchangeRate.USD_out}
           </Text>
-          <Text style={styles.text}>s
+          <Text style={styles.text}>
             EUR/BYN: {exchangeRate.EUR_in} / {exchangeRate.EUR_out}
           </Text>
-          <SvgUri
-            style={styles.svg}
-            uri={`https://belarusbank.by${exchangeRate.flag}`}
-            width="50"
-            height="50"
-          />
-          <Text style={styles.text}>Enter amount in USD:</Text>
+          <Text style={styles.text}>
+            RUB/BYN: {exchangeRate.RUB_in} / {exchangeRate.RUB_out}
+          </Text>
+          <Picker
+            selectedValue={selectedCurrency}
+            onValueChange={handleCurrencyChange}
+            style={styles.picker}
+          >
+            <Picker.Item label="USD" value="USD" />
+            <Picker.Item label="EUR" value="EUR" />
+            <Picker.Item label="RUB" value="RUB" />
+          </Picker>
+          <Text style={styles.text}>Enter amount:</Text>
           <TextInput
             style={styles.input}
             value={amount}
@@ -78,6 +97,9 @@ const styles = StyleSheet.create({
     width: '80%',
     fontSize: 24,
     textAlign: 'center',
+  },
+  picker: {
+    width: '80%',
   },
 });
 
